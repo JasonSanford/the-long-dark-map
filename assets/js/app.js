2d2ae54d49799288e372896f6a4e0ea4bbbe8b27
Jason Sanford <jasonsanford@gmail.com>
2016-01-27 20:43:43 -0500

    Use mapbox-gl-js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
-var map = new L.Map('map');
+mapboxgl.accessToken = 'pk.eyJ1IjoiamNzYW5mb3JkIiwiYSI6InRJMHZPZFUifQ.F4DMGoNgU3r2AWLY0Eni-w';
 
-map.setView([0.00668, 0.00757], 15);
-
-var tileUrl = 'https://api.mapbox.com/styles/v1/jcsanford/cijna760h00id93ly3eqhr7ah/tiles/{z}/{x}/{y}?access_token=sk.eyJ1IjoiamNzYW5mb3JkIiwiYSI6ImNpam9lMXNqNzAwdTl1Zm01OXp0OWR0eGcifQ.1UnpWThNOderQiG8cNvaYA';
-var tileLayer = new L.TileLayer(tileUrl, {minZoom: 14});
-tileLayer.addTo(map);
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/jcsanford/cijna760h00id93ly3eqhr7ah',
+  center: [0.00668, 0.00757],
+  zoom: 15
+});
