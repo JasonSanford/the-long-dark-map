dbb62d3def3c3cd0f1ed78930f2d016d4233a951
Jason Sanford <jasonsanford@gmail.com>
2016-01-29 15:07:32 -0500

    Zoomto

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,10 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiamNzYW5mb3JkIiwiYSI6InRJMHZPZFUifQ.F4DMGoNgU3
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/jcsanford/cijna760h00id93ly3eqhr7ah',
-  center: [0.00668, 0.00757],
-  zoom: 15
+  center: [0.00794800, 0.00677284],
+  zoom: 12
 });
+
+setTimeout(function () {
+  map.zoomTo(15)
+}, 1000);
