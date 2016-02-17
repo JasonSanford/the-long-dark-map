c0e998c8198e6c4b1a263569ce94c0f963a9757a
Jason Sanford <jasonsanford@gmail.com>
2016-02-17 10:01:40 -0500

    Start location details.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,72 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiamNzYW5mb3JkIiwiYSI6InRJMHZPZFUifQ.F4DMGoNgU3r2AWLY0Eni-w';
 
+var initialPosition = [0.00794800, 0.00677284];
+var initialZoom = 15;
+
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/jcsanford/cijna760h00id93ly3eqhr7ah',
-  center: [0.00794800, 0.00677284],
-  zoom: 12
+  center: initialPosition,
+  zoom: initialZoom - 3,
+  minZoom: 14,
+  maxZoom: 18
 });
 
 setTimeout(function () {
-  map.zoomTo(15)
+  map.zoomTo(initialZoom)
 }, 1000);
+
+function locationHovered(event) {
+  var $section = $(event.target).closest('.location');
+  var layer = $section.data('layer');
+
+  $section[event.type === 'mouseenter' ? 'addClass' : 'removeClass']('hovered');
+
+  $section.siblings('.location').each(function (i, o) {
+    map.setPaintProperty($(o).data('layer'), 'text-color', '#000');
+    map.setPaintProperty($(o).data('layer'), 'text-halo-color', '#fff');
+  });
+
+  if (event.type === 'mouseenter') {
+    map.setPaintProperty(layer, 'text-color', '#818e86');
+    map.setPaintProperty(layer, 'text-halo-color', '#31393a');
+  } else {
+    map.setPaintProperty(layer, 'text-color', '#000');
+    map.setPaintProperty(layer, 'text-halo-color', '#fff');
+  }
+}
+
+function locationClicked(event) {
+  var $section = $(event.target).closest('.location');
+  var layer = $section.data('layer');
+
+  if ($section.hasClass('selected')) {
+    $section.removeClass('selected').find('.location-detail').hide();
+    map.setPaintProperty(layer, 'text-color', '#000');
+    map.setPaintProperty(layer, 'text-halo-color', '#fff');
+    map.flyTo({
+      center: initialPosition,
+      zoom: initialZoom
+    });
+  } else {
+    $section.siblings('.location').each(function (i, o) {
+      $(o).removeClass('selected').find('.location-detail').hide();
+      map.setPaintProperty($(o).data('layer'), 'text-color', '#000');
+      map.setPaintProperty($(o).data('layer'), 'text-halo-color', '#fff');
+    });
+    $section.addClass('selected').find('.location-detail').show();
+    map.setPaintProperty(layer, 'text-color', '#818e86');
+    map.setPaintProperty(layer, 'text-halo-color', '#31393a');
+    map.flyTo({
+      center: [$section.data('y'), $section.data('x')],
+      zoom: 16
+    });
+  }
+}
+
+$('.location').hover(
+  locationHovered,
+  locationHovered
+);
+
+$('.location').on('click', locationClicked);
